393074c9738e5e8f75bec8095a47d2fab8dae47c
Ankit Rai <ankitrai800@gmail.com>
2023-03-29 17:29:40 +0530

    set homepage to dashboard component

---
+++ src/app/trending/trending.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TrendingComponent } from './components/trending/trending.component';
+
+
+
+@NgModule({
+  declarations: [
+    TrendingComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class TrendingModule { }
