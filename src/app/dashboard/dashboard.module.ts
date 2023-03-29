d2cb084f1be1200b516cac4d2ca6470ecd21ad43
Ankit Rai <ankitrai800@gmail.com>
2023-03-29 17:52:34 +0530

    import sidebar module into dashboard

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { Routes, RouterModule } from '@angular/router';
+import { SidebarModule } from '../sidebar/sidebar.module';
 
 const routes: Routes = [
   {
@@ -16,6 +17,7 @@ const routes: Routes = [
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-  ]
+    SidebarModule,
+  ],
 })
 export class DashboardModule { }
