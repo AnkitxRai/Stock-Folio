5854bca355c14cb6597cf81d10a767b236ca9a11
Ankit Rai <ankitrai800@gmail.com>
2023-05-11 15:14:57 +0530

    fix header

--- src/app/header/components/header/header.component.ts
+++ src/app/header/components/header/header.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  public form!: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.setupForm();
+    console.log(this.form);
+  }
+
+  setupForm() {
+    this.form = new FormGroup({
+      search: new FormControl()
+    });
+  }
+
+  getValue(event: Event) {
+    timer(2000).subscribe(() => {
+      console.log((event.target as HTMLInputElement).value);
+    })
+  }
 
 }
--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -9,7 +9,9 @@ import { HeaderComponent } from './components/header/header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     HeaderComponent
